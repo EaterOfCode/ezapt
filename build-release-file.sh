493095057900b0fbedfd17b7a7fbaf1c4d07e121
Corne Oppelaar <hello@eaterofco.de>
2016-07-25 23:48:07 +0200

    Add release file and move dists

---
+++ build-release-file.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+cat > ./dists/daily/Release <<EOF
+Origin: EaterOfCode
+Label: EaterOfCode
+Suite: Main
+Date: $(date +"%a, %d %b %Y %X %Z")
+SHA256:
+EOF
+
+cd ./dists/daily;
+find * -type f -print0 | while read -d '' -r file; do
+if [ "$file" != "Release" ] && [ "$(basename "$file")" != ".gitkeep" ]; then
+        echo " $(sha256sum "$file" | awk '{print $1}') $(stat --printf="%s" "$file") $file" >> Release;
+    fi
+done
--- build.sh
+++ build.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
+DIR="$(realpath "$(dirname $0)")";
 
 echo "Building ${1}";
 mkdir .result;
 docker build -t eater/ezapt .;
 docker run -v $(realpath ./.result):/result -v $(realpath .):/ezapt eater/ezapt bash /ezapt/run.sh "$1";
-mv -f ./.result/*.deb "./repo/binary/${1}.deb";
-cd ./dists/main;
+mv -f ./.result/*.deb "./dists/daily/main/binary/${1}.deb";
+cd ./dists/daily/main/binary;
 dpkg-scanpackages binary /dev/null | gzip -9c > binary/Packages.gz;
+cd "$DIR";
 rm -rf .result;
+./build-release-file.sh;
